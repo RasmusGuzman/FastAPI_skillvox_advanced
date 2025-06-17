96d3941608c4fcf877db44103d56e73fbe8e1a9e
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 12:18:57 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,16 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from typing import Annotated
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-LocalSession = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+# Объявляем явно тип LocalSession
+LocalSession = Annotated[
+    sessionmaker[AsyncSession],
+    {"bind": engine, "class_": AsyncSession, "expire_on_commit": False},
+]
 
 Base = declarative_base()
 
 def get_session() -> AsyncSession:
     session = LocalSession()
     return session
-

bac0e0a5048aaeb511c248009d0bf6a605b04575
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 16:14:13 +0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text, JSON
+from sqlalchemy import JSON, Column, Integer, String, Text
 
 from .database import Base
-    
+
 
 class Recipe(Base):
     __tablename__ = "recipes"
-    
-    id: Column[int] = Column(Integer, primary_key=True, index=True)
-    name: Column[str] = Column(String(100), nullable=False)
-    views_count: Column[int] = Column(Integer, default=0)
-    preparation_time: Column[int] = Column(Integer, nullable=False)
-    ingredients: Column[dict] = Column(JSON)
-    description: Column[str] = Column(Text)
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    views_count = Column(Integer, default=0)
+    preparation_time = Column(Integer, nullable=False)
+    ingredients = Column(JSON)
+    description = Column(Text)

0394b2ed6eb4838c858c6a9229bef814fc85c8e7
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 11:04:53 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,14 +1,15 @@
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from src.main import app
-from src.database import Base
-from src.database_for_tests import test_engine, TestingSessionLocal, prepare_database
+from src.database import Base, get_session
+from src.database_for_tests import test_engine, TestingSessionLocal
 
-
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def pre_database():
-    await prepare_database()
-    yield
+@pytest_asyncio.fixture(autouse=True, scope="session")
+def _override_get_session():
+    async def _get_test_session():
+        async with TestingSessionLocal() as session:
+            yield session
+    app.dependency_overrides[get_session] = _get_test_session
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
 async def prepare_database():
@@ -19,6 +20,7 @@ async def prepare_database():
     async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
+
 @pytest_asyncio.fixture
 async def async_session():
     async with TestingSessionLocal() as session:
@@ -28,8 +30,6 @@ async def async_session():
 @pytest_asyncio.fixture
 
 async def ac():
-
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         yield ac
-

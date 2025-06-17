1dafda5a64b45e9a44e64a993d9b95cd812ac2ae
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 11:06:48 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import pytest
 from httpx import AsyncClient
 
 @pytest.mark.asyncio
-async def test_read_root(ac: AsyncClient, prepare_database):
+async def test_read_root(ac: AsyncClient):
     response = await ac.get("/")
     assert response.status_code == 200
     assert "<h1>Welcome to best recipes library!!</h1>" in response.text
@@ -10,7 +10,7 @@ async def test_read_root(ac: AsyncClient, prepare_database):
 
 
 @pytest.mark.asyncio
-async def test_create_recipe(aс, prepare_database):
+async def test_create_recipe(ac: AsyncClient):
     response = await ac.post("/recipes/", json={
         "name": "Борщ",
         "preparation_time": 30,
@@ -22,20 +22,20 @@ async def test_create_recipe(aс, prepare_database):
     assert data["name"] == "Борщ"
 
 @pytest.mark.asyncio
-async def test_list_recipes(ac, prepare_database):
+async def test_list_recipes(ac: AsyncClient):
     response = await ac.get("/recipes/")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
 
 @pytest.mark.asyncio
-async def test_nonexistent_recipe(ac, prepare_database):
+async def test_nonexistent_recipe(ac: AsyncClient):
     response = await ac.get("/recipes/99999")
     assert response.status_code == 404
     assert response.json()["detail"] == "Recipe not found"
 
 @pytest.mark.asyncio
-async def test_homepage(ac, prepare_database):
+async def test_homepage(ac: AsyncClient):
     response = await ac.get("/")
     assert response.status_code == 200
     assert "html>" in response.text.split()

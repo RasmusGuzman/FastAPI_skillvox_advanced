570853bc722dd9789eba3060157a97effe74f3ea
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 11:04:30 +0300

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -3,14 +3,16 @@ from pydantic import BaseModel
 class BaseRecipe(BaseModel):
     name: str
     preparation_time: int
-    ingredients: str
+    ingredients: list[str]
     description: str
 
 class RecipeIn(BaseRecipe):
     ...
 
-class RecipeOut(BaseRecipe):
-    ...
+class RecipeOut(BaseModel):
+    name: str
+    views_count: int
+    preparation_time: int
 
     class Config:
         orm_mode = True

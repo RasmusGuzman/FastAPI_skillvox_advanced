d9acc65774aa82dca426757ff74d468cc0ab351a
RasmusGuzman <160243458+RasmusGuzman@users.noreply.github.com>
2025-06-17 11:03:41 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, AsyncGenerator
 
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from starlette.responses import HTMLResponse
 
@@ -20,24 +21,20 @@ async def startup():
         await conn.run_sync(models.Base.metadata.create_all)
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.session.close()
-    await database.engine.dispose()
-
 
 @app.post("/recipes/", response_model=schemas.RecipeOut)
-async def create_recipe(recipe_in: schemas.RecipeIn) -> models.Recipe:
+async def create_recipe(recipe_in: schemas.RecipeIn,
+                        session: AsyncSession = Depends(database.get_session)) -> models.Recipe:
     new_recipe = models.Recipe(**recipe_in.dict())
-    async with database.session as sess:
+    async with session as sess:
         sess.add(new_recipe)
         await sess.commit()
     return new_recipe
 
 
 @app.get("/recipes/", response_model=List[schemas.AllRecipeOut])
-async def list_recipes():
-    result = await database.session.execute(
+async def list_recipes(session: AsyncSession = Depends(database.get_session)):
+    result = await session.execute(
         select(models.Recipe).order_by(
             models.Recipe.views_count.desc(), models.Recipe.preparation_time
         )
@@ -45,40 +42,28 @@ async def list_recipes():
 
     my_base = result.scalars().all()
 
-    return [
-        {
-            "name": recipe.name,
-            "views_count": recipe.views_count,
-            "preparation_time": recipe.preparation_time,
-        }
-        for recipe in my_base
-    ]
+    return [schemas.AllRecipeOut.from_orm(recipe) for recipe in my_base]
 
 
 @app.get("/recipes/{recipe_id}", response_model=schemas.RecipeOut)
-async def get_recipe(recipe_id: int):
-    result = await database.session.execute(
+async def get_recipe(recipe_id: int,
+                     session: AsyncSession = Depends(database.get_session)):
+    result = await session.execute(
         select(models.Recipe).where(models.Recipe.id == recipe_id)
     )
     recipe = result.scalar_one_or_none()
-    if recipe is None:
 
+    if not recipe:
         raise HTTPException(status_code=404, detail="Recipe not found")
-    else:
-        recipe.views_count += 1
-        async with database.session as sess:
-            await sess.commit()
 
-        data = {
-            "name": recipe.name,
-            "preparation_time": recipe.preparation_time,
-            "ingredients": recipe.ingredients,
-            "description": recipe.description,
-        }
+    recipe.views_count += 1
+    await session.commit()
+    await session.refresh(recipe)
 
-    return data
+    return recipe
 
 
 @app.get("/", response_class=HTMLResponse)
-async def async_start(request: Request):
+async def async_start(request: Request,
+                      session: AsyncSession = Depends(database.get_session)):
     return templates.TemplateResponse("base.html", {"request": request})
